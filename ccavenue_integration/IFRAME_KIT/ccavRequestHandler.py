3383749f1bf40714c82dfd9c01cb6b99dfc7c0db
viralpatel15 <viralkansodiya167@gmail.com>
2024-08-13 05:42:22 +0000

    Enable CCAvenue Setting

--- ccavenue_integration/IFRAME_KIT/ccavRequestHandler.py
+++ ccavenue_integration/IFRAME_KIT/ccavRequestHandler.py
@@ -20,38 +20,39 @@ def ccav_response_handler():
 @frappe.whitelist(allow_guest=True)
 def ccav_request_handler(form_data):
     doc = frappe.get_doc("CCAvenue Settings")
-    access_code = doc.access_code
-    WORKING_KEY = doc.working_key
-    ACCESS_CODE = doc.access_code
-    MERCHANT_CODE = doc.merchant_code
-    REDIRECT_URL = doc.redirect_url
-    CANCEL_URL = doc.cancel_url
+    if doc.enable:
+        access_code = doc.access_code
+        WORKING_KEY = doc.working_key
+        ACCESS_CODE = doc.access_code
+        MERCHANT_CODE = doc.merchant_code
+        REDIRECT_URL = doc.redirect_url
+        CANCEL_URL = doc.cancel_url
 
-    my_string = WORKING_KEY
-    
-    key = my_string
+        my_string = WORKING_KEY
         
-    encrypted_data = encrypt(form_data, key)
+        key = my_string
+            
+        encrypted_data = encrypt(form_data, key)
 
-    import requests
+        import requests
 
-    url = "https://apitest.ccavenue.com/apis/servlet/DoWebTrans"
+        url = "https://apitest.ccavenue.com/apis/servlet/DoWebTrans"
 
-    payload = {
-        "request_type": "JSON",
-        "access_code": ACCESS_CODE,
-        "command": "generateQuickInvoice",
-        "version": "1.2",
-        "response_type": "JSON",
-        "enc_request": encrypted_data
-    }
-    try:
-        response = requests.post(url, data=payload, headers={})
-        response = response.text.split('=')[2]
-        data = decrypt(response, key)
-        return data
-    except Exception as e:
-        frappe.log_error(e)
+        payload = {
+            "request_type": "JSON",
+            "access_code": ACCESS_CODE,
+            "command": "generateQuickInvoice",
+            "version": "1.2",
+            "response_type": "JSON",
+            "enc_request": encrypted_data
+        }
+        try:
+            response = requests.post(url, data=payload, headers={})
+            response = response.text.split('=')[2]
+            data = decrypt(response, key)
+            return data
+        except Exception as e:
+            frappe.log_error(e)
 
 
 def caesar_cipher_encrypt(text, shift):
