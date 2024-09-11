dad38a12e02813ee26e6ac7c2c6f455c6a63b37c
root <viral@fosserp.com>
2024-09-11 07:48:50 +0000

    ch

--- ccavenue_integration/ccavenue_integration/quotation.py
+++ ccavenue_integration/ccavenue_integration/quotation.py
@@ -6,7 +6,6 @@ def before_submit(self, method):
     if frappe.db.get_single_value('CCAvenue Settings', 'enable'):
         doc = frappe.get_doc("CCAvenue Settings")
         form_data = {
-                "merchant_id": doc.merchant_code,
                 "customer_id": self.party_name,
                 "customer_name": self.customer_name,
                 "customer_email_id": self.contact_email,
@@ -14,18 +13,14 @@ def before_submit(self, method):
                 "valid_for": 2,
                 "valid_type": "days",
                 "currency": self.currency,
-                "bill_delivery_type":"EMAIL",
                 "amount": self.grand_total,
-                "customer_mobile_no": self.contact_mobile,
                 "merchant_reference_no": self.name,
-                "sub_acc_id": "sub1",
+                "merchant_reference_no1": self.name,
+                "merchant_reference_no2": self.name,
+                "merchant_reference_no3": self.name,
+                "merchant_reference_no4": self.name,
+                "sub_acc_id": "",
                 "terms_and_conditions": "terms and condition",
-                "due_date": "3",
-                "late_payment_fees": "0",
-                "late_payment_fees_type": "Perc",
-                "discount_if_paid_within_due_date": "4",
-                "discount_value": "0",
-                "discount_type": "Perc",
                 "sms_content": "PlspayyourLegalEntity_Namebill#Invoice_IDforInvoice_Currency Invoice_Amount online at Pay_Link."
             }
         form_data = json.dumps(form_data)
