52b774547fa2ea189a229594edcf56b1645aa404
root <viral@fosserp.com>
2024-10-07 12:23:41 +0000

    ch

--- ccavenue_integration/IFRAME_KIT/invoice_lookup.py
+++ ccavenue_integration/IFRAME_KIT/invoice_lookup.py
@@ -11,62 +11,65 @@ from datetime import datetime
 
 
 def get_parameters():
-    doc = frappe.get_doc("Quotation", "SAL-QTN-2024-00015")
+    get_quotation_list = frappe.db.get_list("Quotation", {"custom_payment_status" : "Unpaid", "custom_ccavenue_invoice_id" : ["!=" , ""]}, pluck="name")
 
-    from_date = doc.transaction_date.strftime('%d-%m-%Y')
+    for row in get_quotation_list:
+        doc = frappe.get_doc("Quotation", row)
 
-    to_date = getdate().strftime('%d-%m-%Y')
+        from_date = doc.transaction_date.strftime('%d-%m-%Y')
 
-    form_data = {
-        "from_date": from_date,
-        "to_date": to_date,
-        "invoice_id": doc.custom_ccavenue_invoice_id,
-        "page_count": "1"
-        }
+        to_date = getdate().strftime('%d-%m-%Y')
 
-    doc = frappe.get_doc("CCAvenue Settings")
-    if doc.enable:
-        access_code = doc.access_code
-        WORKING_KEY = doc.working_key
-        ACCESS_CODE = doc.access_code
-        MERCHANT_CODE = doc.merchant_code
-        REDIRECT_URL = doc.redirect_url
-        CANCEL_URL = doc.cancel_url
+        form_data = {
+            "from_date": from_date,
+            "to_date": to_date,
+            "invoice_id": doc.custom_ccavenue_invoice_id,
+            "page_count": "1"
+            }
 
-        my_string = WORKING_KEY
-        
-        key = my_string
+        doc = frappe.get_doc("CCAvenue Settings")
+        if doc.enable:
+            access_code = doc.access_code
+            WORKING_KEY = doc.working_key
+            ACCESS_CODE = doc.access_code
+            MERCHANT_CODE = doc.merchant_code
+            REDIRECT_URL = doc.redirect_url
+            CANCEL_URL = doc.cancel_url
+
+            my_string = WORKING_KEY
             
-        encrypted_data = encrypt(form_data, key)
+            key = my_string
+                
+            encrypted_data = encrypt(form_data, key)
 
-        import requests
-        print("enc : " , encrypted_data)
-        
-        url = "https://api.ccavenue.com/apis/servlet/DoWebTrans"
+            import requests
+            print("enc : " , encrypted_data)
+            
+            url = "https://api.ccavenue.com/apis/servlet/DoWebTrans"
 
-        payload = {
-            "request_type": "JSON",
-            "access_code": ACCESS_CODE,
-            "command": "invoiceList",
-            "version": "1.1",
-            "response_type": "JSON",
-            "enc_request": encrypted_data
-        }
+            payload = {
+                "request_type": "JSON",
+                "access_code": ACCESS_CODE,
+                "command": "invoiceList",
+                "version": "1.1",
+                "response_type": "JSON",
+                "enc_request": encrypted_data
+            }
 
-        try:
-            response = requests.post(url, data=payload, headers={})
-            
-            print("CCAvenue :", response)
-            print("text : ", response.text)
-            
-            response = response.text.split('=')[2]
-            
-            print(response)
-            
-            data = decrypt(response, key)
-            
-            return data
-        except Exception as e:
-            frappe.log_error(response)
-            frappe.log_error(e)
+            try:
+                response = requests.post(url, data=payload, headers={})
+                
+                print("CCAvenue :", response)
+                print("text : ", response.text)
+                
+                response = response.text.split('=')[2]
+                
+                print(response)
+                
+                data = decrypt(response, key)
+                
+                return data
+            except Exception as e:
+                frappe.log_error(response)
+                frappe.log_error(e)
 
