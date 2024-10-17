dd7dffee8ac0c4d1fb2a3ec8edaf36c67f5d8333
root <viral@fosserp.com>
2024-10-17 12:17:06 +0000

    ch

--- ccavenue_integration/IFRAME_KIT/get_item_list.py
+++ ccavenue_integration/IFRAME_KIT/get_item_list.py
@@ -47,9 +47,105 @@ def get_items():
         print(json_data["Invoice_Item_Result"]["item_List"]["item"])
 
         for row in json_data["Invoice_Item_Result"]["item_List"]["item"]:
-            if frappe.db.exists("Item", row['name']):
-                continue
-            else:
-                print(row['name'])
+            if row['type'] == "ITEM":
+                if frappe.db.exists("Item", row['name']):
+                    continue
+                else:
+                    print(row['name'])
 
-        # return data
+        # return dataNBrand logo
+
+
+# Myntra Account Management
+# Nykaa Account Management
+# Amazon Lisitng
+# Mesho Onboarding
+# Copyright
+# PMEGP
+# CITUS
+# ASPIRE
+# LMCS for MSMEs
+# International Cooperation
+# Marketing Assistance Scheme
+# RMA against Bank Guarantee
+# GST Reg. Amendment
+# IEC Registration
+# IEC Reg. Amendment
+# GST Monthly under 1Cr
+# GST Monthly with reconunder1Cr
+# GST Monthlywith recon 1 to 5Cr
+# GST Monthly with recon 5to10Cr
+# GST Monthly for annual
+# GST Quarterly under 1Cr
+# GST Quarterly for annual
+# E invoicing Onboarding
+# Amazon.com onboarding
+# Pernia Account Management
+# Aza Account Management
+# Intl 6 platforms for 1 year
+# Intl 6 platforms onboarding
+# TQUS
+# Single Point Reg Scheme
+# ISEC
+# Government fees
+# Bundle Package Intermediate
+# Ecommerce Bundle package
+# BP Preparation More than 1 Cr
+# One to One Expert Advisory
+# Digital Sol Standard Plan
+# Digital Sol Customised Plan
+# Beautypreneur Skin and Hair
+# BP Skin And Hair And Advance
+# Intermediate Webinar
+# DSC
+# eCommerce website
+# Account Manager
+# Brnad logo
+# Basic Beauty Skill Training
+# Social Social Media Marketing
+# Standard Website Development
+# C40  Bronze Social Media Optim
+# Bronze Social Media optm
+# Website eCommerce
+# Trade mark certificate
+# Additional gov .fees
+# Firm audit
+# Amazon .com
+# Amazon onboard
+# Purplle Account Management
+# Tata Cliq Account Management
+# Tata Platte Account Management
+# ECommerce Package Full Program
+# ECommerce Package Intermediate
+# GST Custom Advisory
+# Company Registration Pvt Ltd
+# Company Registration LLP
+# TAN
+# PAN
+# Udyam
+# FSSAI
+# Shop and Establishment
+# Trademark
+# compliance Turnover Less 1Cr
+# compliance Turnover 1 To 5Cr
+# compliance Turnover 5 to 10Cr
+# Annual Returns and Compliance
+# Tata Cliq Onboarding
+# Aza Fashions Onboarding
+# MirrawLux Onboarding
+# Ajio Account Management
+# MirrawLux Account Management
+# Karma Place Account Management
+# Ogaan Account Management
+# Jaipore Account Management
+# GST Composition scheme 1to 5Cr
+# GST Composition scheme 5to10Cr
+# GST Composition scheme annual
+# ECommerce Package Advance
+# ECommerce Package Pro 1to1
+# 1 MG Onboarding
+# Co Reg Partnership Firm
+# E invoicing SAAS
+# Myntra onboarding and listing
+# BP Preparation 20 Lakhs
+# Beautypreneur Skin Services
