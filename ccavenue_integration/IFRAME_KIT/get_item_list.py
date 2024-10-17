ec582d110b77444a49b7edf47b9e7268ab695349
root <viral@fosserp.com>
2024-10-17 12:14:34 +0000

    ch

--- ccavenue_integration/IFRAME_KIT/get_item_list.py
+++ ccavenue_integration/IFRAME_KIT/get_item_list.py
@@ -47,9 +47,9 @@ def get_items():
         print(json_data["Invoice_Item_Result"]["item_List"]["item"])
 
         for row in json_data["Invoice_Item_Result"]["item_List"]["item"]:
-            if frappe.db.exists("Item", row.name):
+            if frappe.db.exists("Item", row['name']):
                 continue
             else:
-                print(row.name)
+                print(row['name'])
 
         # return data
