02c01381ace71add13c058e6afc0ec732de9cd6d
root <viral@fosserp.com>
2024-10-18 07:15:43 +0000

    ch

--- ccavenue_integration/IFRAME_KIT/generate_invoice.py
+++ ccavenue_integration/IFRAME_KIT/generate_invoice.py
@@ -4,11 +4,11 @@ import json
 
 def get_quotation(doc, due_date):
     form_data = {
-            "customer_name": doc.customer_name,
-            "customer_email_id": doc.contact_email,
+            "customer_name": "{0}".format(doc.customer_name),
+            "customer_email_id": "{0}".format(doc.contact_email),
             "customer_email_subject": "Invoice",
-            "customer_mobile_no": doc.contact_mobile,
-            "currency": doc.currency,
+            "customer_mobile_no": "{0}".format(doc.contact_mobile),
+            "currency": "{0}".format(doc.currency),
             "valid_for": "2",
             "valid_type": "days",
             "item_List": [],
