202861759cc548fa35878ad242307914d18bbeb3
root <viral@fosserp.com>
2024-10-28 10:05:23 +0000

    ch

--- ccavenue_integration/IFRAME_KIT/generate_invoice.py
+++ ccavenue_integration/IFRAME_KIT/generate_invoice.py
@@ -27,7 +27,7 @@ def get_quotation(doc):
                 "name": "{0}".format(row.item_code),
                 "description": "{0}".format(row.item_code),
                 "quantity": str(row.qty),
-                "unit_cost": row.rate,
+                "unit_cost": str(row.rate),
                 "tax_List": [
                     {
                     "name": "CGST",
