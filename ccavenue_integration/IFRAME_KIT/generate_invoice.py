a9ffacbd83686cbd320a444a437fb1fc59a903ad
root <viral@fosserp.com>
2024-10-30 04:43:06 +0000

    ch

--- ccavenue_integration/IFRAME_KIT/generate_invoice.py
+++ ccavenue_integration/IFRAME_KIT/generate_invoice.py
@@ -111,7 +111,7 @@ def get_quotation(doc):
     #     "sms_content": "Pls payyourLegalEntity_Namebill#Invoice_IDfor Invoice_Currency Invoice_Amount online at Pay_Link."
     # }
     print(type(form_data))
-    data = json.dumps(form_data)
+    # data = json.dumps(form_data)
     print(data)
     response = ccav_request_handler(data, "generateInvoice")
 
