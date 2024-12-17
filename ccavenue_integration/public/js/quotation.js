ddcdb252935c6af73d1069772c87bd757f642e18
root <viral@fosserp.com>
2024-12-17 10:03:30 +0000

    ch

--- ccavenue_integration/public/js/quotation.js
+++ ccavenue_integration/public/js/quotation.js
@@ -1,6 +1,6 @@
 frappe.ui.form.on('Quotation', {
     refresh:frm=>{
-        if(!frm.doc.taxes_and_charges){
+        if(!frm.doc.taxes_and_charges && frm.doc.docstatus < 1){
             frm.set_value("taxes_and_charges", 'Output GST In-state - BC')
         }
     }
