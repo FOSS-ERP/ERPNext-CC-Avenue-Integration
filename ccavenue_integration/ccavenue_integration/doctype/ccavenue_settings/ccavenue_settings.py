b1a20d6a9b6a5d876a0f672e33c7543708d8f2c7
viralkansodiya <viral@fosserp.com>
2025-05-06 15:15:30 +0000

    delay the function to return the value

--- ccavenue_integration/ccavenue_integration/doctype/ccavenue_settings/ccavenue_settings.py
+++ ccavenue_integration/ccavenue_integration/doctype/ccavenue_settings/ccavenue_settings.py
@@ -19,7 +19,7 @@ class CCAvenueSettings(Document):
 		quotation = doc.items[0].prevdoc_docname
 
 		qo_doc = frappe.get_doc("Quotation", quotation)
-		time.sleep(2)
+
 		return process_full_payment_invoice(qo_doc)
 
 		
